fd680066301199ffb345bf0f1845b647cfc1150d
songruirui <willsongrui@gmail.com>
2013-08-01 15:18:05 +0800

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,17 @@
 class PagesController < ApplicationController
   def index
+  	@title = "index"
   end
 
   def about
+  	@title = "About"
   end
   
   def home
+  	@title = "Home"
   end
 
   def contact
+  	@title = "Contact"
   end
 end
